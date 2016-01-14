739b06e1db6bf1c619830b0392a3217e3ebbc88d
LeonG001 <lgrobbel@cut.ac.za>
2016-01-14 21:53:21 +0200

    Diabetes classes completed.

--- Impilo App/ReportQueries/Format 2/Diabetes/Diabetes2Q.cs
+++ Impilo App/ReportQueries/Format 2/Diabetes/Diabetes2Q.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+
 namespace Impilo_App.ReportQueries.Format_2.Diabetes
 {
     class Diabetes2Q
