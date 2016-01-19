8141d994b9bffc3d03540534f6c3624ab03a1aac
wynandn <wynandn@gmail.com>
2016-01-19 19:00:19 +0200

    Clinic Classes

---
+++ Impilo App/LocalModels/ClinicHypertension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impilo_App.LocalModels
+{
+    class ClinicHypertension
+    {
+        public int chID { get; set; }
+
+        public int EncounterID { get; set; }
+
+        public bool chDWFReferral { get; set; }
+
+        public decimal chDiagAndTreatSystolic { get; set; }
+
+        public decimal chDiagAndTreatDiastolic { get; set; }
+
+        public decimal chNotOnMedsSystolic { get; set; }
+
+        public decimal chNotOnMedsDiastolic { get; set; }
+
+        public DateTime chNextReviewDate { get; set; }
+
+        public decimal chOnMedsSystolic { get; set; }
+
+        public decimal chOnMedsDiastolic { get; set; }
+
+        public string chBloodSugarLevel { get; set; }
+
+        public string chCreatinine { get; set; }
+
+        public string chCholesterol { get; set; }
+
+        public DateTime chDateOfVisit { get; set; }
+    }
+}
---
+++ Impilo App/LocalModels/ClinicMaternalHealth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impilo_App.LocalModels
+{
+    class ClinicMaternalHealth
+    {
+        public int cmhID { get; set; }
+
+        public int EncounterID { get; set; }
+
+        public bool cmhDWFReferral { get; set; }
+
+        public bool cmhMomConnectRegistered { get; set; }
+
+        public string cmhANCVisitNo { get; set; }
+
+        public string cmhPNC1Week { get; set; }
+
+        public bool cmhPCRDone { get; set; }
+
+        public string cmhPNC6Week { get; set; }
+
+        public DateTime cmhDateOfVisit { get; set; }
+    }
+}
